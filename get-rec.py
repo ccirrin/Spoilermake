c696cc0daf731e9b024b362fbdb2ec5f5a0df3f4
Daniel <danielrferriss@gmail.com>
2018-10-20 10:01:13 -0500

    get recommendations

--- get-rec.py
+++ get-rec.py
@@ -1,10 +1,13 @@
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
-import spotipy.util as util
+import json
+import requests
 import pandas as pd
+
 
 client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+sp.trace=False
 
 df = pd.read_csv("test.csv")
 
@@ -47,6 +50,11 @@ minValence = df['valence'].min()
 seedGenres = df['artist genres']
 seedArtists = df['artist']
 
+test_list = []
+test_list.append(str(targetDance.item()))
+print(test_list)
+
 #listOfRecs = sp.recommendations(seed_artists=seedArtists, seed_genres=seedGenres, seed_tracks=None, limit=5, country=None, min_acousticness=minAcoustic, max_acousticness=maxAcoustic, target_acousticness=targetAcoustic, min_danceability=minDance, max_danceability=maxDance, target_danceability=targetDance, min_energy=minEnergy, max_energy=maxEnergy, target_energy=targetEnergy, min_instrumentalness=minInstrument, max_instrumentalness=maxInstrument, target_instrumentalness=targetInstrument, min_liveness=minLive, max_liveness=maxLive, target_liveness=targetLive, min_loudness=minLoud, max_loudness=maxLoud, target_loudness=targetLoud, min_speechiness=minSpeech, max_speechiness=maxSpeech, target_speechiness=targetSpeech, min_tempo=minTempo, max_tempo=maxTempo, target_tempo=targetTempo, min_valence=minValence, max_valence=maxValence, target_valence=targetValence)
-listOfRecs = sp.recommendations(seed_artists=None, seed_genres=None, seed_tracks='spotify:track:1iLfJK5MYfn6SWuPOZa539')
-#print(listOfRecs)
+listOfRecs = sp.recommendations(seed_artists=['spotify:artist:70cRZdQywnSFp9pnc2WTCE'], target_danceability=test_list, limit=20)
+print(listOfRecs)
+
